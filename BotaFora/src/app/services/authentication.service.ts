00c3ee65a36fcaf79437c0184cd1ccb4017b6a64
Matheus Hahn <matheus.hahn@sap.com>
2019-09-19 23:58:07 -0300

    Create authentication service

---
+++ BotaFora/src/app/services/authentication.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { LoginCredential } from '../interfaces/LoginCredential';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  constructor(private _angularFireAuth: AngularFireAuth) {}
+
+  registerUser(credencials: LoginCredential): Promise<firebase.auth.UserCredential> {
+    return this._angularFireAuth.auth.createUserWithEmailAndPassword(
+      credencials.email, 
+      credencials.password
+    );
+  }
+
+  login(credencials: LoginCredential): Promise<firebase.auth.UserCredential> {
+    return this._angularFireAuth.auth.signInWithEmailAndPassword(
+      credencials.email,
+      credencials.password
+    );
+  }
+
+  logout(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      if (this._angularFireAuth.auth.currentUser) {
+        this._angularFireAuth.auth.signOut()
+        .then(() => {
+          resolve();
+        }).catch((error) => {
+          reject();
+        });
+      }
+    })
+  }
+}
