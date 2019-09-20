7a1c1a3f05dcf55c13676a46c83834d2d5e22cba
Matheus Hahn <matheus.hahn@sap.com>
2019-09-19 23:57:38 -0300

    Add npm packages for firebase

--- BotaFora/src/app/app.module.ts
+++ BotaFora/src/app/app.module.ts
@@ -6,13 +6,21 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule
